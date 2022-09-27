17d0650103b5f028d2d5c8bef6f6e0ae3f3b3eb5
Ken <y23@mail.com>
2022-09-27 18:01:54 +0100

    Complete code

--- primes.py
+++ primes.py
@@ -2,5 +2,18 @@
 """ENTER YOUR SOLUTION HERE!"""
 
 def primes(number_of_primes):
-    list = []
+    if number_of_primes <= 0:
+        raise ValueError
+    
+    list = [2]
+    num = 3
+    
+    while len(list) < number_of_primes:
+        isPrime = True
+        for i in range(2, num):
+            if num % i == 0:
+                isPrime = False
+        if isPrime:
+            list.append(num) 
+        num += 1       
     return list
